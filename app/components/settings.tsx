5187397c639235a0490ce343fd7a02ca501765e8
sethvoler <353619470@qq.com>
2024-01-29 23:44:00 +0800

    feat: 更改title & 隐藏版本更新

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -692,7 +692,7 @@ export function Settings() {
             </Popover>
           </ListItem>
 
-          <ListItem
+          {/* <ListItem
             title={Locale.Settings.Update.Version(currentVersion ?? "unknown")}
             subTitle={
               checkingUpdate
@@ -715,7 +715,7 @@ export function Settings() {
                 onClick={() => checkUpdate(true)}
               />
             )}
-          </ListItem>
+          </ListItem> */}
 
           <ListItem title={Locale.Settings.SendKey}>
             <Select
